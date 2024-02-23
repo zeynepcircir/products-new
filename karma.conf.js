1bb6bee80d9e5842a414c7c5edd8df04dbc6ffe5
zeynepcircir <81877446+zeynepcircir@users.noreply.github.com>
2024-02-23 15:00:12 +0300

    Config

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/product-list'),
+      dir: require('path').join(__dirname, './coverage/product-crud'),
       subdir: '.',
       reporters: [
         { type: 'html' },
